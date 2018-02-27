07f208c65dd4709cd05cbad69e9fef86b2392582
Steven Blank <steven.blank@kellogg.northwestern.edu>
2018-02-26 19:53:39 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+// React Poker
+
+// GOAL
+// When the application is launched, show five face down cards, and a button that
+// reads "Deal a new hand". Each time the "deal a new hand" button is clicked, show
+// five random, unique playing cards.
+
+// HINTS
+// - The initial state of the state object is already set for you.
+// - You can create an array of JSX elements and add them to the return of the render() function in
+// - one line. The framework for doing this is already there.
+// - Add an element to an array by using the .push() function
+// - Grab the few n elements of an array by using the .splice() function, e.g. myArray.splice(0, n)
+// - The card images follow this convention: http://golearntocode.com/images/cards/8_of_diamonds.png
+// - Calling shuffledDeck() will give you an array of 52 strings, representing the rank and suit of
+//   each card, in random order
+// - Start by getting the five face down cards to show, then use the React developer tools in Chrome
+//   to manipulate state manually, then write an event handler function and corresponding button.
+import React, { Component } from 'react';
+import shuffledDeck from './ShuffledDeck';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hand: ["face_down", "face_down", "face_down", "face_down", "face_down"]
+    }
+  }
+
+  dealCards(){
+    this.setState({
+      cards: shuffledDeck.splice(0,47),
+    })
+  }
+  render() {
+    // array to hold JSX elements
+    let cardsImage = "http://golearntocode.com/images/cards/" + this.state.hand + ".png";
+    let handImage = "http://golearntocode.com/images/cards/" + this.state.cards + ".png";
+    let cards = ["http://golearntocode.com/images/cards/" + this.state.cards + ".png"];
+    return (
+      <div className="App">
+      <h1>
+      <img src= {cardsImage}/>
+      <img src= {handImage}/>
+      </h1>
+        <p><button>{()=>this.dealCards()}>Deal New Cards</button></p>
+      </div>
+    );
+  }
+}
+
+export default App;
